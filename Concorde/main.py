d0fe693637b3f2556a13aa51d9c285bb6d5ac6ac
Simon Bayle <simon.bayle@sia-partners.com>
2024-12-24 17:30:27 +0100

    Final concorde integration

--- Concorde/main.py
+++ Concorde/main.py
@@ -4,11 +4,11 @@ from tsp_concorde import main_solve
 
 def main():
     path_file = os.getcwd() + '/Sujet-Données/Instance.xlsx'
-    path_file = "/mnt/c/Users/SimonBAYLE/OneDrive - Sia Partners/Documents/training/Concorde/Combinatorial-optimization-TSPTW-VRPTW-/Sujet-Données/Instance.xlsx"
+    path_file = ""
     dist = lecteur(path_file)
 
     main_solve(distance_matrix=dist,
-               path_concorde="/mnt/c/Users/SimonBAYLE/OneDrive - Sia Partners/Documents/training/Concorde/Combinatorial-optimization-TSPTW-VRPTW-/Concorde/Concorde_exe")
+               path_concorde=os.getcwd() + "/Concorde_exe")
 
 
 if __name__=="__main__": # la fonction main est lancée avec cette expression particulière
